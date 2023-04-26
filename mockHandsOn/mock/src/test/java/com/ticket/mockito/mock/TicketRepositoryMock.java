5baa4cd051ae3408585dcf6255d65b654c392593
ram_kiren <ramkiren.rs@gmail.com>
2023-04-26 12:32:28 +0530

    Update TicketRepositoryMock.java

--- mockHandsOn/mock/src/test/java/com/ticket/mockito/mock/TicketRepositoryMock.java
+++ mockHandsOn/mock/src/test/java/com/ticket/mockito/mock/TicketRepositoryMock.java
@@ -4,18 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TicketRepositoryMock implements TicketRepository {
 	
-	int saveCalled = 0;
-	Ticket lastAddedTicket = null;
-	
-	@Override
-	public void save(Ticket ticket) {
-		saveCalled++;
-		lastAddedTicket = ticket;
-	}
-	
-	public void verify(Ticket ticket, int times){
-		assertEquals(times, saveCalled);
-		assertEquals(ticket, lastAddedTicket);
-	}
+
 	
 }
